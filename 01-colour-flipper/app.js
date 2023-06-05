d0d51f9cf4e8edb620f6e6f7665d9e11a5825d56
JackCT1 <jackthomas933@gmail.com>
2023-06-05 21:36:29 +0100

    simple colour app working

--- 01-colour-flipper/app.js
+++ 01-colour-flipper/app.js
@@ -1,1 +1,13 @@
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+const button = document.getElementById("btn");
+const color = document.querySelector(".color");
 
+button.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.innerHTML = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
