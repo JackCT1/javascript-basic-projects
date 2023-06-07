fa975d0d5f8ac461fdd8195a9f68302e52e84bd6
JackCT1 <jackthomas933@gmail.com>
2023-06-07 11:27:00 +0100

    all buttons working

--- 02-Counter/app.js
+++ 02-Counter/app.js
@@ -1,1 +1,18 @@
+const countValue = document.getElementById("value");
+const buttons = document.querySelectorAll(".btn");
 
+let count = 0;
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    const styles = event.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    countValue.textContent = count;
+  });
+});
